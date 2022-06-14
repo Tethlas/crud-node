157ff7a2e01bfb0fe12c3a48518261f44b65ffbb
fumagallilaura <la_12_msn@hotmail.com>
2022-06-14 08:48:59 -0300

    chore: config eslint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
-import express from "express"
+import express from 'express';
 
-const app = express()
+const app = express();
 
+const port = 3000;
 
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
