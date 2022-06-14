afe102163e1f0d393630aede64d4bffaed48a1c2
fumagallilaura <la_12_msn@hotmail.com>
2022-06-14 10:53:03 -0300

    feat: add service and controller files

---
+++ src/services/userService.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/extensions */
+import { errorObjects } from '../schemas/usersValidations';
+import validations from '../schemas/geneticValidations';
+import userModel from '../models/userModel.js';
+
+const getAll = () => {
+
+};
+
+export default {
+  getAll,
+};
